54258368e402bcf370770ffcf04b4b6aa07446a7
Yrutan <simon.roy.26@hotmail.com>
2015-10-13 20:17:23 -0400

    drop down list pour afficher la sélection des cours
    je vais tester ça avec le serveur mecredi le 14.

--- view/view_assigner_plancadre.php
+++ view/view_assigner_plancadre.php
@@ -16,6 +16,71 @@
            <link rel="Stylesheet" href="../assets/styles.css">
            <link rel="Stylesheet" href="../assets/others.css">
         </head>
+        <script type="text/javascript">
+
+        // Author: Justin Whitford
+        // Source: www.evolt.org
+        // http://evolt.org/node/55035/
+
+        /*
+            CBX_Filter(filter, list)
+            filter : est une chaine de charactère qui va servir de filtre
+            list : est une référence à un objet de type select (ComboBox)
+
+            La liste est réorganisée pour que les éléments qui contiennent le filtre se retrouvent
+            en haut de la liste.
+
+            exemple : <input type="text" name="uneZoneTexte" onchange="CBX_filter(this.value, this.form.CBX_nom)">
+            <select name="CBX_select">
+            <option></option>
+            <option value="valeurVoulue" ><*option>
+        */
+        function CBX_Filter(filter, list)
+        {
+            //si on n'a pas déjà un backup de la liste
+            //en faire un maintenant
+            if(!list.backUp)
+            {
+                list.backUp = new Array();
+                for(i = 0; i < list.length; i++)
+                {
+                    list.backUp[list.backUp.length] = new Array(list[i].value, list[i].text);
+                }
+            }
+            match = new Array();
+            nomatch = new Array();
+
+            for(i = 0; i < list.backUp.length; i++)
+            {
+                if(list.backUp[i][1].toLowerCase().indexOf(filter.toLowerCase()) != -1)
+                {
+                    // ajouter l'élément à la fin de l'array
+                    match[match.length] = new Array(list.backUp[i][0], list.backUp[i][1]);
+                }
+                else
+                {
+                    nomatch[nomatch.length] = new Array(list.backUp[i][0], list.backUp[i][1]);
+                }
+            }
+
+            // ajoute les éléments qui contiennent le filtre en premier
+            for(i = 0; i < list.backUp.length; i++)
+            {
+                list[i].value = match[n][0];
+                list[i].text = match[n][1];
+            }
+            // ajoute les éléments qui ne contiennent pas le filtre après ceux qui le contiennent
+            for(i = 0; i < nomatch.length; i++)
+            {
+                list[i+match.length].value = nomatch[n][0];
+                list[i+match.length].text = nomatch[n][1];
+            }
+
+            //pour qu'on voit immédiatement que la liste a été modifiée lorsqu'on utilise le filtre
+            list.selectedIndex = 0;
+        }
+
+        </script>
 
 
         <div class="container">
@@ -29,10 +94,10 @@
             Choisir un utilisateur :
             </br>
 
-            <input></input>
+            <input name=""></input>
 
 
-            <select>
+            <select name="CBX_ListeCours">
                 <?php
                     $array = fillComboBoxUser();
                     for (int i = 0; i < count($array); i++)
