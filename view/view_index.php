a6c230af503d493c324c77c4ccf76baf5aa8ef61
AntoineLatendresse <a.latendresse2@hotmail.com>
2015-10-23 13:38:30 -0400

    Début de la page web acceuil
    Laisser moi vos commentaires et on va avoir besoin d'une photo de
    chacun!

--- view/view_index.php
+++ view/view_index.php
@@ -22,8 +22,52 @@
             ?>
       <br>
 
-      <div>Petit test</div>
-      
+          <fieldset>
+              <legend>Accueil :</legend>
+              </br>
+
+              <form>
+                    <p>
+                        Dans le but de compléter notre programme d’étude, nous avons choisi de faire un projet qui viendra en aide à un problème que l’administration rencontre souvent.
+                    <br><br>
+                        Le projet consiste à offrir une interface pour guider l’utilisateur dans l’élaboration d’un plan-cadre. L’interface offrira à l’usager un pas-à-pas avec des instructions afin qu’aucune partie du plan-cadre ne soit oubliée. Le texte des instructions devra pouvoir être modifié par un administrateur du site.
+                    <br><br>
+                        Bref, le projet plan-cadre vise à faciliter et à simplifier l'élaboration et la gestion des plans-cadres. Dans le but ultime d’améliorer le développement et de faciliter l’accès aux plans-cadres.
+                    </p>
+
+              </form>
+          </fieldset>
+          <fieldset>
+              <legend>Développeurs :</legend>
+              </br>
+
+
+              <img style='float:left; margin-right: 4px;' src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+              <img style='float:left; height: 24px; width: 24px; margin-right: 4px;' src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+              <img style='float:left; height: 24px; width: 24px; margin-right: 4px;' src="pic_mountain.jpg" alt="Mountain View" style="width:304px;height:228px;">
+              <br><br>
+              <h2 style='float:left; margin-right: 25px;'>Antoine Latendresse</h2>
+              <h2 style='float:left; margin-right: 25px;'>Simon Roy</h2>
+              <h2 style='float:left; margin-right: 25px;'>Léa Kelly</h2>
+
+
+          </fieldset>
+
+          <fieldset>
+              <legend>Superviseur :</legend>
+              </br>
+
+              <h2 style='float:left; margin-right: 25px;'>Saliha Yacoub</h2>
+
+          </fieldset>
+
+          <fieldset>
+              <legend>Dédier à :</legend>
+              </br>
+              <h2 style='float:left; margin-right: 25px;'>Johanne Raymond</h2>
+              <h2 style='float:left; margin-right: 25px;'>Renaud Thibodeau</h2>
+          </fieldset>
+
       </div>
 
      </body>
