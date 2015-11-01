23f3fcc52924408de9e77819b48052bad7efc930
Yrutan <simon.roy.26@hotmail.com>
2015-11-01 14:50:23 -0500

    création d'un fichier pour les constantes
    effacer deux fichiers textes mal placés et ajout d'un dossier pour avoir
    un backup de la BD

---
+++ assets/constant.php
@@ -0,0 +1,31 @@
+<?php
+/* 
+	constant.php définie toutes les constantes qui 
+	seront utilitées par le site.
+
+*/
+
+define("LOGOUT", "../conroller/controller_logout.php");
+
+define("VIEW_LOGIN", "../view/view_login.php");
+define("VIEW_CREATE_ACCOUNT", "../view/view_create_account.php");
+
+define("VIEW_INDEX", "../view/view_index.php");
+
+// données à entrer
+define("VIEW_CREATE_PROGRAM", "../view/view_create_program.php");
+define("VIEW_CREATE_CLASS", "../view/view_create_class.php");
+define("VIEW_CREATE_COMPETENCE", "../view/view_create_competence.php");
+
+// plancadre
+define("VIEW_ASSIGN_USER", "../view/view_assign_user.php");
+define("VIEW_ELABORATION_PLANCADRE", "../view/view_elaboration_plancadre.php");
+define("VIEW_CREATE_PLANCADRE", "../view/view_create_plancadre.php");
+
+
+
+
+
+
+
+
