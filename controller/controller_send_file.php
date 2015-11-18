b200db501e170886fd16e10d6f3b00ad7d1e2c97
Yrutan <simon.roy.26@hotmail.com>
2015-11-17 22:45:38 -0500

    update avec phpWord
    arrive à envoyer un fichier créer mais n'arrive pas à faire fonctionner
    le template.
    
    Liste de chose à faire demain, préférablement avant la rencontre,
    1. trouver comment inclure $PHPWord->loadTemplate()
    2. ajouter des bookmarks pour "remplacer" les variables
    3. modifier les variables du template pour leur ajouter un $ au début
    
    Sinon, on a comme option d'utiliser un objet COM mais pour ça il faut
    modifier certains paramètres dans php.ini et avoir Microsoft Office
    d'installer. On pourra le tester demain.

--- controller/controller_send_file.php
+++ controller/controller_send_file.php
@@ -10,36 +10,30 @@
 // trouver comment changer le nom en fonction du plancadre choisi
 
 
-// use phpWord ?
-// http://phpword.codeplex.com/
-
-require_once '../assets/PHPWord.php';
-
-$file = '../assets/template.dotx';
 
 
-$php_word = new PHPWord();
 
-// After creating a section, you can append elements:
-$section->addText('Hello world!')
-// You can directly style your text by giving the addText function an array:
-$section->addText('Hello world! I am formatted.', array('name'=>'Tahoma', 'size'=>16, 'bold'=>true));
-
-// If you often need the same style again you can create a user defined style to the word document
-// and give the addText function the name of the style:
-$php_word->addFontStyle('myOwnStyle', array('name'=>'Verdana', 'size'=>14, 'color'=>'1B2232'));
-$section->addText('Hello world! I am formatted by a user defined style', 'myOwnStyle');
-
-// You can also putthe appended element to local object an call functions like this:
-$myTextElement = $section->addText('Hello World!');
-$myTextElement->setBold();
-$myTextElement->setName('Verdana');
-$myTextElement->setSize(22);
-
-$writer = PHPWord_IOFactory::createWriter($php_word, 'Word2007');
-$writer->save('../plancadre/testWord.docx');
+// placer des bookmark dans le document Word
 
 
+// plus d'objet COM exemple
+/*
+com_load_typelib('Word.Application');
+$word = new COM("word.application");
+$word->Documents->Open('../plancadre/Doc1.doc');
+$word->Visible = 1;
+$word->ActiveDocument->FormFields("Text1")->Result = "something";
+$word->ActiveDocument->Close(false);
+$word->Quit();
+unset($word);
+*/
+
+
+
+
+
+////////////////////////////////////////////////////////////////
+// code pour envoyer un fichier au client (download)
 /*
 
 if (file_exists($file)) {
@@ -56,6 +50,123 @@ if (file_exists($file)) {
 
 */
 
+////////////////////////////////////////////////////////////////
+
+
+// exemple a tester 
+
+
+// montre comment utiliser objet COM
+// semble être mieux que les autres
+// faire un essai sur un ordi qui possede office
+// http://www.phpbuilder.com/columns/yunus20031124.php3?page=2
+
+// http://www.phpbuilder.com/columns/venkatesan20030501.php3?page=2
+// modifier les permissions sur le pc pour permettre à l'application d'être utiliser
+// et doit modifier php.ini pour
+/* 
+[com]
+; allow Distributed-COM calls com.allow_dcom = true ;
+autoregister constants of a components typlib on com_load() com.autoregister_typelib = true ; 
+register constants casesensitive ;
+com.autoregister_casesensitive = false ; 
+show warnings on duplicate constat registrations com.autoregister_verbose = true 
+*/
+/*
+
+//1. Instanciate Word
+$word = new COM("word.application") or die("Unable to instantiate Word");
+//2. specify the MS Word template document (with Bookmark TODAYDATE inside)
+$template_file = "../assets/template.docx";
+//3. open the template document
+$word->Documents->Open($template_file);
+//4. get the current date MM/DD/YYYY
+$current_date = date("m/d/Y");
+
+//5. get the bookmark and create a new MS Word Range (to enable text substitution)
+$bookmark_code_cours = "{code_cours}";
+$objBookmark = $word->ActiveDocument->Bookmarks($bookmark_code_cours);
+$range = $objBookmark->Range;
+//6. now substitute the bookmark with actual value
+$range->Text = $current_date;
+
+
+//7. save the template as a new document (c:/reminder_new.doc)
+$new_file = "../plancadre/template.docx";
+$word->Documents[1]->SaveAs($new_file);
+//8. free the object
+$word->Quit();
+$word->Release();
+$word = null; 
+
+*/
+
+
+
+////////////////////////////////////////////////////////////////////////////
+require_once '../assets/PHPWord.php';
+
+$document = $PHPWord->loadTemplate('../assets/template.docx');
+
+
+$row = array('cours',
+  'ponderation',
+  'prealable');
+
+// doit peut-être créer des bookmarks
+// à regarder demain avant la rencontre
+// les variables ont peut-être besoin de $ en avant
+// par exemple ${nomvariable}
+
+$document->setValue('code_cours', $row[1]);
+$document->setValue('ponderation_cours', $row[2]);
+$document->setValue('prealable_cours', $row[3]);
+
+$file = '../plancadre/test.docx';
+
+$document->save($file);
+
+/*
+// use phpWord ?
+// http://phpword.codeplex.com/
+// exemple de PHPWORD pour créer un document
+// fonctione correctement, si rien ne fonctionne
+// on va probablement devoir s'en servir pour créer
+// le template avec du code ce qui n'est pas idéal si
+// on veut pouvoir le changer sans changer le code
+// (à moins qu'on fasse une interface pour ça ? pas une bonne idée)
+require_once '../assets/PHPWord.php';
+
+$file = '../assets/template.dotx';
+
+// Create a new PHPWord Object
+$PHPWord = new PHPWord();
+
+// Every element you want to append to the word document is placed in a section. So you need a section:
+$section = $PHPWord->createSection();
+
+// After creating a section, you can append elements:
+$section->addText('Hello world!');
+
+// You can directly style your text by giving the addText function an array:
+$section->addText('Hello world! I am formatted.', array('name'=>'Tahoma', 'size'=>16, 'bold'=>true));
+
+// If you often need the same style again you can create a user defined style to the word document
+// and give the addText function the name of the style:
+$PHPWord->addFontStyle('myOwnStyle', array('name'=>'Verdana', 'size'=>14, 'color'=>'1B2232'));
+$section->addText('Hello world! I am formatted by a user defined style', 'myOwnStyle');
+
+// You can also putthe appended element to local object an call functions like this:
+$myTextElement = $section->addText('Hello World!');
+//$myTextElement->setBold();
+//$myTextElement->setName('Verdana');
+//$myTextElement->setSize(22);
+
+// At least write the document to webspace:
+$objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
+$objWriter->save('../plancadre/helloWorld.docx');
+
+*/
 
 
 
@@ -67,6 +178,7 @@ if (file_exists($file)) {
 
 
 
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -77,6 +189,7 @@ if (file_exists($file)) {
 
 
 
+////////////////////////////////////////////////////////////////////////////////
 
 // autre solution, utiliser un objet COM
 
@@ -93,13 +206,14 @@ if (file_exists($file)) {
 
 
 
+/////////////////////////////////////////////////////
 
+// trop de code pour très peu de bénéfices
+// (savoir clairement ce qu'on envoie)
 
 // on pourrait utiliser la fonction http_send_file($file)
 // la fonction retourne un boolean donc peut-être 
 // qu'il faut l'appeler dans une page normale
-
-
 
 // pour le type des MIME :
 // avec tous les MIME de Office 2007
