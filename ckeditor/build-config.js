7117a198957752c28393af79188facd3707f1da9
AntoineLatendresse <201087112@P104-07.clg.priv>
2015-11-25 10:15:05 -0500

    EditText

--- ckeditor/build-config.js
+++ ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/d005177101b58a13bb20440fb2bdd266
+ * (2) http://ckeditor.com/builder/f9e61f3c2caec04b82035da07cf5d9a1
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/d005177101b58a13bb20440fb2bdd266
+ * (3) http://ckeditor.com/builder/download/f9e61f3c2caec04b82035da07cf5d9a1
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'icy_orange',
-	preset: 'full',
+	preset: 'basic',
 	ignore: [
 		'.bender',
 		'bender.js',
@@ -51,48 +51,14 @@ var CKBUILDER_CONFIG = {
 	],
 	plugins : {
 		'basicstyles' : 1,
-		'bidi' : 1,
-		'blockquote' : 1,
 		'clipboard' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
-		'contextmenu' : 1,
-		'dialogadvtab' : 1,
-		'div' : 1,
-		'elementspath' : 1,
 		'enterkey' : 1,
-		'find' : 1,
+		'entities' : 1,
 		'floatingspace' : 1,
 		'font' : 1,
-		'format' : 1,
-		'forms' : 1,
-		'horizontalrule' : 1,
-		'iframe' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
 		'justify' : 1,
-		'language' : 1,
-		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
-		'magicline' : 1,
-		'maximize' : 1,
-		'pagebreak' : 1,
-		'pastefromword' : 1,
-		'pastetext' : 1,
-		'removeformat' : 1,
-		'resize' : 1,
-		'scayt' : 1,
-		'selectall' : 1,
-		'showblocks' : 1,
-		'showborders' : 1,
-		'smiley' : 1,
-		'specialchar' : 1,
-		'stylescombo' : 1,
-		'tab' : 1,
-		'table' : 1,
-		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'wsc' : 1,
